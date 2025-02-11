5015c3546aed38f6995dc138787cd845e7d25eec
meadowse <baha.199620@gmail.com>
2025-02-11 12:10:39 +0300

    fix

--- src/botMattermost/webhookPlugin.py
+++ src/botMattermost/webhookPlugin.py
@@ -60,8 +60,8 @@ class webhookPlugin(Plugin):
         """Прослушивает веб-перехватчики «ping» и «pong» и либо обновляет исходный пост,
         либо отправляет сообщение на канал, чтобы указать, что веб-перехватчик работает."""
         if event.body.get('user_name') == event.context.get("manager_nickname"):
-            set_value_by_id('T209', 'F4491', 'Провал', event.context.get("doc_id"))
-            set_value_by_id('T209', 'F4529', 'NULL', event.context.get("doc_id"))
+            set_value_by_id('T209', 'F4491', 'Провал', event.context.get("kp_id"))
+            set_value_by_id('T209', 'F4529', 'NULL', event.context.get("kp_id"))
             if isinstance(event, ActionEvent):
                 self.driver.respond_to_web(
                     event,
